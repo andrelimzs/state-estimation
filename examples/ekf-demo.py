ccb21a2728656ded3e95d8c68b9e059009169d93
andrelimzs <andrelimzs@outlook.com>
2022-03-26 23:50:24 +0800

    ekf-demo : Working ekf demo

--- examples/ekf-demo.py
+++ examples/ekf-demo.py
@@ -20,11 +20,15 @@
 import sys
 sys.path.append("..")
 
-import numpy as np
-import numpy.linalg as LA
+# import numpy as np
+# import numpy.linalg as LA
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import control
+
+import autograd.numpy as np
+import autograd.numpy.linalg as LA
+from autograd import grad
 
 
 # -
@@ -60,22 +64,16 @@ def eqn(t,y,ref):
     
     # Unpack control input
     u = ref(t)
-    alpha = u
-    T = 1
-    gravity = -1
+    alpha = u[0]
+    T = [1]
+    gravity = 0
     
     dydt = np.zeros(6)
-    # dx = vx
     dydt[0] = vx + gravity
-    # dy = vy
     dydt[1] = vy
-    # dtheta = omega
     dydt[2] = omega
-    # dvx = cos(theta + alpha) T
-    dydt[3] = np.cos(theta + alpha) * T
-    # dvy = sin(theta + alpha) T
-    dydt[4] = np.sin(theta + alpha) * T
-    # domega = -sin (alpha) T
+    dydt[3] = np.cos(theta + alpha + 0.1*np.random.rand()) * T + 0.2*np.random.rand()
+    dydt[4] = np.sin(theta + alpha + 0.1*np.random.rand()) * T + 0.2*np.random.rand()
     dydt[5] = -np.sin(alpha) * T
     
     return dydt
@@ -92,39 +90,62 @@ def simulate_system(t_span, ref, Ts, y0=None):
     return sol.t, sol.y, ref(sol.t)
 
 def ref_sine(t):
-    return ( np.sin(t) + np.sin(3*t) ).reshape((-1))
+    alpha = ( np.sin(t) - np.sin(3*t) ).reshape((-1))
+    ref = np.stack([ alpha, np.ones_like(alpha) ])
+    return ref
 
 
 # +
 N = 6
-Ts = 0.01
+Ts = 1/400
 
 t, y_true, u_true = simulate_system([0,10], ref_sine, Ts)
 
 # Add noise
-y = y_true + np.random.randn(*y_true.shape) * np.array([0.5, 0.5, 0.1, 0.1, 0.01, 0.01]).reshape(-1,1)
-u = u_true + 0.1 * np.random.randn(*u_true.shape)
+y = y_true + np.random.randn(*y_true.shape) * np.array([0.5, 0.5, 0.1, 0.05, 0.05, 0.01]).reshape(-1,1)
+u = u_true + 0 * np.random.randn(*u_true.shape)
 
-# +
-fig, axes = plt.subplots(N//2, 2, figsize=(12,9), dpi=100)
-ax = axes.T.flatten()
 
-labels = ['x', 'y', r'$\theta$', 'vx', 'vy', r'$\omega$']
-units  = ['m', 'm', 'deg', 'm/s', 'm/s', 'deg/s']
-for i,a in enumerate(ax):
-    a.plot(t, y[i,:])
-    a.plot(t, y_true[i,:], 'k--')
-    # a.plot(t, estimate[i,:])
+# + tags=[]
+def plot_system(t, y, y_true, estimate1=None, estimate2=None, dpi=None):
+    fig, axes = plt.subplots(2,3, figsize=(16,6), dpi=dpi)
+    ax = axes.flatten()
+
+    labels = ['x', 'y', r'$\theta$', 'vx', 'vy', r'$\omega$']
+    units  = ['m', 'm', 'rad', 'm/s', 'm/s', 'rad/s']
+    for i,a in enumerate(ax):
+        a.plot(t, y[i,:])
+        a.plot(t, y_true[i,:], 'k--')
+        if estimate1 is not None:
+            a.plot(t, estimate1[i,:])
+        if estimate2 is not None:
+            a.plot(t, estimate2[i,:])
+
+        a.set_ylabel(f'{labels[i]} ({units[i]})')
+        # a.set_title(labels[i])
+        a.grid()
+
+    axes[0,0].legend(['measurement', 'true', 'estimate'], loc='upper left')
+    [ axes[1,i].set_xlabel('time (s)') for i in range(3) ]
+
+    fig.tight_layout()
+
+def plot_control_input(u, u_true, dpi=None):
+    fig, axes = plt.subplots(1, 2, figsize=(12,3), dpi=dpi)
+    ax = axes.T.flatten()
+
+    for i,a in enumerate(ax):
+        a.plot(t, u[i,:])
+        a.plot(t, u_true[i,:], 'k--')
+        a.grid()
+
+    axes[0].set_ylabel(r'$\alpha$ (rad)')
+    axes[1].set_ylabel('T (norm)')
+    axes[0].set_xlabel('time (s)')
+    axes[1].set_xlabel('time (s)')
     
-    a.set_ylabel(f'{labels[i]} ({units[i]})')
-    a.set_title(labels[i])
-    a.grid()
-    
-ax[0].legend(['measurement', 'true', 'estimate'], loc='upper left')
-ax[2].set_xlabel('time (s)')
-ax[5].set_xlabel('time (s)')
-
-fig.tight_layout()
+plot_system(t, y, y_true, dpi=300)
+plot_control_input(u, u_true)
 # -
 
 # ## Initialise and Use Kalman Filter
@@ -132,35 +153,56 @@ fig.tight_layout()
 # +
 # %%time
 from estimation.KalmanFilter import KalmanFilter
+from estimation.ExtendedKalmanFilter import ExtendedKalmanFilter
 
 # # Instantiate Kalman Filter
-H = np.eye(N)
+A = np.array([[ 0,0,0, 1,0,0 ],
+              [ 0,0,0, 0,1,0 ],
+              [ 0,0,0, 0,0,1 ],
+              [ 0,0,0, 0,0,0 ],
+              [ 0,0,0, 0,0,0,],
+              [ 0,0,0, 0,0,0 ]])
+B = np.array([ [0,0], 
+               [0,0],
+               [0,0],
+               [0,1],
+               [1,0],
+               [-1,0] ])
+
+H = np.diag([ 0,0,0,1,1,1 ])
 # Process Covariance
-Q = 1e-2 * np.eye(N)
+Q = 0.1 * np.diag([ 1,1,1,1,1,1 ])
 # Observation Covariance
-R = 1e2 * np.eye(N)
+R = 1e2 * np.diag([ 1,1,1, 1,1,0.1 ])
+
+
+def F(x,y,theta,vx,vy,omega,alpha,T):
+    alpha = 0
+    T = 0
+    # State is { x,y,theta, vx,vy,omega }
+    dydt = []
+    dydt.append( vx )
+    dydt.append( vy )
+    dydt.append( omega )
+    dydt.append( np.cos(theta + alpha) * T )
+    dydt.append( np.sin(theta + alpha) * T )
+    dydt.append(-np.sin(alpha) * T )
+    return np.stack(dydt)
 
 kf = KalmanFilter.from_continuous(A,H,Q,R,Ts,B=B)
+ekf = ExtendedKalmanFilter(Ts,N,2,F,H,Q,R)
 
-estimate = np.zeros_like(y)
+estimate_kf = np.zeros_like(y)
+estimate_ekf = np.zeros_like(y)
 for i in range(y.shape[1]):
     measurement = y[:,i]
-    control_input = u[i:i+1]
+    control_input = u[:,i]
     # print(f"meas: {measurement.shape} \t u: {control_input.shape}")
-    estimate[:,i] = kf(measurement, control_input)
+    estimate_kf[:,i] = kf(measurement, control_input)
+    estimate_ekf[:,i] = ekf(measurement, control_input)
     # kf(measurement, control_input)
 
-# Unpack
-est_pos = estimate[0,:].T
-est_vel = estimate[1,:].T
+plot_system(t, y, y_true, estimate_kf, estimate_ekf, dpi=300)
+# -
 
-fig,axes = plt.subplots(2, N//2, figsize=(12,6), dpi=100)
-fig.tight_layout()
-for i,ax in enumerate(axes.flatten()):
-    ax.plot(t, y[i,:])
-    ax.plot(t, y_true[i,:], 'k--')
-    ax.plot(t, estimate[i,:])
-    ax.grid()
-    
-axes[0,2].legend(['measurement', 'true', 'estimate'], loc='upper right')
-_ = [ axes[1,i].set_xlabel('time (s)') for i in range(3) ]
+
